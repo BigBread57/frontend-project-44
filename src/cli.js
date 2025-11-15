bd9fe4db30c7b8e237c75d4777cfe57d55b3339f
Gleb Mazur <g.mazur@samolet.ru>
2025-11-15 11:18:40 +0300

    fix: правка замечаний

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync'
 
 const getName = () => {
   const name = readlineSync.question('May I have your name? ')
-  console.log(`Hello ${name}!`)
+  console.log(`Hello, ${name}`)
 }
 
 export default getName
